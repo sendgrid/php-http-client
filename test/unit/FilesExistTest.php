f181ad5fa353eed1afe5622d247659937eb5a978
Elmer Thomas <elmer@thinkingserious.com>
2018-01-23 14:11:00 -0800

    Fix test

--- test/unit/FilesExistTest.php
+++ test/unit/FilesExistTest.php
@@ -15,6 +15,7 @@ class FilesExistTest extends \PHPUnit_Framework_TestCase
         $this->assertFileExists("$rootDir/CHANGELOG.md");
         $this->assertFileExists("$rootDir/CODE_OF_CONDUCT.md");
         $this->assertFileExists("$rootDir/CONTRIBUTING.md");
+        $this->assertFileExists("$rootDir/Dockerfile");
         $this->assertFileExists("$rootDir/.github/ISSUE_TEMPLATE");
         $this->assertFileExists("$rootDir/LICENSE.txt");
         $this->assertFileExists("$rootDir/.github/PULL_REQUEST_TEMPLATE");
@@ -25,9 +26,6 @@ class FilesExistTest extends \PHPUnit_Framework_TestCase
 
         #$composeExists = file_exists('./docker-compose.yml') || file_exists('./docker/docker-compose.yml');
         #$this->assertTrue($composeExists);
-
-        $dockerExists = file_exists('./Dockerfile') || file_exists('./docker/Dockerfile');
-        $this->assertTrue($dockerExists);
     }
 }
 
