91cd91a12f805c57f09f240b1b7f63a9bebe16a6
MartijnMelchers <martijn@melchers.nu>
2018-04-09 23:35:39 +0200

    Added alot of methods to the Client.php file, might be a better way to do this but I couldn't find it. Missed any? Please let me know!

--- lib/Client.php
+++ lib/Client.php
@@ -14,6 +14,11 @@
 namespace SendGrid;
 
 /**
+ *
+ * Class Client
+ * @package SendGrid
+ * @version 3.9.6
+ *
  * Quickly and easily access any REST or REST-like API.
  *
  * @method Response get($body = null, $query = null, $headers = null)
@@ -24,6 +29,139 @@ namespace SendGrid;
  *
  * @method Client version($value)
  * @method Client|Response send()
+ *
+ * Adding all the endpoints as a method so code completion works
+ *
+ * General
+ * @method Client stats()
+ * @method Client search()
+ * @method Client monthly()
+ * @method Client sums()
+ * @method Client monitor()
+ * @method Client test()
+ *
+ * Access settings
+ * @method Client access_settings()
+ * @method Client activity()
+ * @method Client whitelist()
+ *
+ * Alerts
+ * @method Client alerts()
+ *
+ * Api keys
+ * @method Client api_keys()
+ *
+ * ASM
+ * @method Client asm()
+ * @method Client groups()
+ *
+ * Browsers
+ * @method Client browsers()
+ *
+ * Campaigns
+ * @method Client campaigns()
+ * @method Client schedules()
+ * @method Client now()
+ *
+ * Categories
+ * @method Client categories()
+ *
+ * Clients
+ * @method Client clients()
+ *
+ * ContactDB
+ * @method Client contactdb()
+ * @method Client custom_fields()
+ * @method Client lists()
+ * @method Client recipients()
+ * @method Client billable_count()
+ * @method Client count()
+ * @method Client reserved_fields()
+ * @method Client segments()
+ *
+ * Devices
+ * @method Client devices()
+ *
+ * Geo
+ * @method Client geo()
+ *
+ * Ips
+ * @method Client ips()
+ * @method Client assigned()
+ * @method Client pools()
+ * @method Client warmup()
+ *
+ * Mail
+ * @method Client mail()
+ * @method Client batch()
+ *
+ * Mailbox Providers
+ * @method Client mailbox_providers()
+ *
+ * Mail settings
+ * @method Client mail_settings()
+ * @method Client address_whitelist()
+ * @method Client bcc()
+ * @method Client bounce_purge()
+ * @method Client footer()
+ * @method Client forward_bounce()
+ * @method Client forward_spam()
+ * @method Client plain_content()
+ * @method Client spam_check()
+ * @method Client template()
+ *
+ * Partner settings
+ * @method Client partner_settings()
+ * @method Client new_relic()
+ *
+ * Scopes
+ * @method Client scopes()
+ *
+ * Senders
+ * @method Client senders()
+ * @method Client resend_verification()
+ *
+ * Sub Users
+ * @method Client subusers()
+ * @method Client reputations()
+ *
+ * Supressions
+ * @method Client suppressions()
+ * @method Client global()
+ * @method Client blocks()
+ * @method Client bounces()
+ * @method Client invalid_emails()
+ * @method Client spam_reports()
+ * @method Client unsubcribes()
+ *
+ * Templates
+ * @method Client templates()
+ * @method Client versions()
+ * @method Client activate()
+ *
+ * Tracking settings
+ * @method Client tracking_settings()
+ * @method Client click()
+ * @method Client google_analytics()
+ * @method Client open()
+ * @method Client subscription()
+ *
+ * User
+ * @method Client user()
+ * @method Client account()
+ * @method Client credits()
+ * @method Client email()
+ * @method Client password()
+ * @method Client profile()
+ * @method Client scheduled_sends()
+ * @method Client enforced_tls()
+ * @method Client settings()
+ * @method Client username()
+ * @method Client webhooks()
+ * @method Client event()
+ * @method Client parse()
+ *
+ * Missed any? Simply add them by doing: @method Client method()
  */
 class Client
 {
