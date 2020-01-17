aaf42fab5396dc13e2e311efdc150efce0eaca7c
childish-sambino <sharrison@twilio.com>
2020-01-17 16:05:47 -0600

    docs: baseline all the templated markdown docs (#139)

--- test/unit/LicenceYearTest.php
+++ test/unit/LicenceYearTest.php
@@ -8,12 +8,12 @@ class LicenceYearTest extends \PHPUnit_Framework_TestCase
     {
         $rootDir = __DIR__ . '/../..';
 
-        $license = explode("\n", file_get_contents("$rootDir/LICENSE.txt"));
+        $license = explode("\n", file_get_contents("$rootDir/LICENSE.md"));
         $copyright = trim($license[2]);
 
         $year = date('Y');
 
-        $expected = "Copyright (c) 2012-{$year} Twilio SendGrid, Inc.";
+        $expected = "Copyright (C) {$year}, Twilio SendGrid, Inc. <help@twilio.com>";
 
         $this->assertEquals($expected, $copyright);
     }
