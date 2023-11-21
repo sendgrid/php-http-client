acaef9afeee292e46bfa5575fbccb960961c4329
Shubham <tiwarishubham635@gmail.com>
2023-11-21 11:12:34 +0530

    feat: added setHost for client (#161)
    * feat: added setHost for client
    
    * fix: Update Client.php

--- lib/Client.php
+++ lib/Client.php
@@ -201,7 +201,7 @@ class Client
      * @var bool
      */
     protected $verifySSLCerts;
-    
+
     /**
      * @var bool
      */
@@ -251,6 +251,20 @@ class Client
     public function getHost()
     {
         return $this->host;
+    }
+
+    /**
+     * Set host
+     *
+     * @param string $host
+     *
+     * @return Client
+     */
+    public function setHost(string $host)
+    {
+        $this->host = $host;
+        
+        return $this;
     }
 
     /**
