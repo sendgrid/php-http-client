f1c7ad9bcc0d6630828402ac704dee7aca556fbd
USAMI Kenta <tadsan@zonu.me>
2025-12-18 18:03:42 +0900

    chore: fix PHP 8.5 curl_close() deprecation warning
    curl_close() is deprecated in PHP 8.5 and has no effect since PHP 8.0, so guard it with PHP_VERSION_ID < 80000.

--- lib/Client.php
+++ lib/Client.php
@@ -529,7 +529,9 @@ class Client
             return $this->retryRequest($responseHeaders, $method, $url, $body, $headers);
         }
 
-        curl_close($channel);
+        if (\PHP_VERSION_ID < 80000) {
+            curl_close($channel);
+        }
 
         return $response;
     }
